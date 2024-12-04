01bde1e9973c84fe5b967364e8fc5ff6f6647a98
StenTheTank <sten11mn@gmail.com>
2024-12-04 17:06:05 +0200

    save to database. read from database. print

--- dags/duckdb_provider_example.py
+++ dags/duckdb_provider_example.py
@@ -7,27 +7,37 @@ Airflow connections to DuckDB in order to use this DAG.
 
 from airflow.decorators import dag, task
 from pendulum import datetime
-import os
-from duckdb_provider.hooks.duckdb_hook import DuckDBHook
-
-CSV_PATH = "include/ducks.csv"
-LOCAL_DUCKDB_CONN_ID = "my_local_duckdb_conn"
-LOCAL_DUCKDB_TABLE_NAME = "duckdb_tables"
+import duckdb
+import pandas as pd
 
 
 @dag(start_date=datetime(2023, 6, 1), schedule=None, catchup=False)
-def duckdb_provider_example():
+def duckdb_tutorial_dag_1():
     @task
-    def query_local_duckdb(my_table):
-        my_duck_hook = DuckDBHook.get_hook(LOCAL_DUCKDB_CONN_ID)
-        conn = my_duck_hook.get_conn()
+    def create_pandas_df():
+        "Create a pandas DataFrame with toy data and return it."
+        ducks_in_my_garden_df = pd.DataFrame(
+            {"colors": ["blue", "red", "yellow"], "numbers": [2, 3, 4]}
+        )
 
-        r = conn.execute(f"SELECT * FROM {my_table};").fetchall()
-        print(r)
+        return ducks_in_my_garden_df
 
-        return r
+    @task
+    def create_duckdb_table_from_pandas_df(ducks_in_my_garden_df):
+        "Create a table in DuckDB based on a pandas DataFrame and query it"
 
-    query_local_duckdb(my_table=LOCAL_DUCKDB_TABLE_NAME)
+        # change the path to connect to a different database
+        conn = duckdb.connect("include/turnover_data.db")
+        conn.sql(
+            f"""CREATE TABLE IF NOT EXISTS ducks_garden AS 
+            SELECT * FROM ducks_in_my_garden_df;"""
+        )
+
+        sets_of_ducks = conn.sql("SELECT numbers FROM ducks_garden;").fetchall()
+        for ducks in sets_of_ducks:
+            print("quack " * ducks[0])
+
+    create_duckdb_table_from_pandas_df(ducks_in_my_garden_df=create_pandas_df())
 
 
-duckdb_provider_example()
+duckdb_tutorial_dag_1()
