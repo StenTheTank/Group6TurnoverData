b0880462de78160f8da7da16a11c056f4ed1c24f
salme <salmeuss@ut.ee>
2024-12-13 08:21:49 +0200

    Comments added

--- dags/Tasks.py
+++ dags/Tasks.py
@@ -9,19 +9,19 @@ import pandas as pd
 import zipfile
 from duckdb_provider.hooks.duckdb_hook import DuckDBHook
 
-DUCKDB_CONN_ID = "my_local_duckdb_conn"
-DUCKDB_TABLE_NAME = "emtak_myygitulu"
+#The path to the data lake where raw downloaded files are stored
 data_lake_dir = os.path.abspath("dataLake")
 
 #First DAG - downloading and processing files from Business register
 @dag(start_date=datetime(2023, 6, 1), schedule=None, catchup=False)
 def business_register_files():
-    #Fisrt task - downloading .zip files
+    #First task - downloading .zip files
     @task
     def downloadfiles():
         base_url = "https://avaandmed.ariregister.rik.ee/et/avaandmete-allalaadimine"
         print(data_lake_dir)
         os.makedirs(data_lake_dir, exist_ok=True)
+        #Searching for specific header to download the .zip files we need
         try:
             response = requests.get(base_url)
             response.raise_for_status()
@@ -47,7 +47,8 @@ def business_register_files():
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
 
-    #Second task - unzip specific files
+    #Second task - unzip specific files. From the downloaded files, we need only two. Here we unzip
+    # the files and save them to a separate folder in the data lake.
     @task
     def unzip_files(download_files):
         partial_names = ["EMTAK", "yldandmed"]
@@ -72,7 +73,8 @@ def business_register_files():
             print(f"No ZIP files containing the specified partial names found in {data_lake_dir}.")
             return {"Extracted files": extracted_files, "dir": extract_dir}
 
-    #Third task - converting .csv files to Pandas dataframe
+    #Third task - converting .csv files to Pandas dataframe. Here we are converting .csv files
+    # to Pandas dataframe, including only necessary columns.
     @task
     def create_pandas_df(unzip_result):
         extracted_files = unzip_result["Extracted files"]
@@ -86,7 +88,8 @@ def business_register_files():
         yldandmed_df = yldandmed_df[columns_to_keep]
         return {"myygitulu": emtak_df, "yldandmed": yldandmed_df}
 
-    #Forth task - creating DB tables
+    #Forth task - creating DB tables. Here we load the data to the database by creating the table
+    #and inserting the data.
     @task
     def create_duckdb_table(dataframes):
         conn = duckdb.connect("include/turnover_data.db")
@@ -138,8 +141,11 @@ def business_register_files():
         print("Top 5 records from yldandmed:", top5_2)
         conn.close()
 
-    # Task Dependencies
+    # Task Dependencies.
     download_files = downloadfiles()
+    #unzip file function doesn't use the given parameter, but it was necessary to add it there
+    #in order to have the tasks triggered sequentially, because we need to have files downloaded
+    #before starting unzipping them.
     unzip_result = unzip_files(download_files)
     dataframes = create_pandas_df(unzip_result)
     create_duckdb_table(dataframes)
@@ -149,7 +155,7 @@ business_register_files()
 #Second DAG - downloading and processing files from tax and customs board
 @dag(start_date=datetime(2023, 6, 1), schedule=None, catchup=False)
 def tax_and_customs_board_files():
-    #First task - downloading .csv files
+    #First task - downloading .csv files from another source.
     @task
     def download_csv_files():
         base_url = "https://www.emta.ee/eraklient/amet-uudised-ja-kontakt/uudised-pressiinfo-statistika/statistika-ja-avaandmed#tasutud-maksud-failid"
@@ -174,7 +180,8 @@ def tax_and_customs_board_files():
                 file.write(file_response.content)
                 print(f"Saved: {file_path}")
 
-    #Second task - creating combined dataframe from .csv files
+    #Second task - creating combined dataframe from .csv files. We downloaded 16 .csv files, and
+    #here we combine them into a single dataframe, keeping only necessary columns.
     @task
     def create_pandas_df(downloaded_files):
         dataframes = []
@@ -192,14 +199,18 @@ def tax_and_customs_board_files():
                     columns_to_keep = ["Registrikood","Nimi","Kaive"]
                     df = pd.read_csv(f, sep=";")
                     df = df[columns_to_keep]
+                    #In order to convert string into float, we first need to remove unnecessary spaces
+                    #and replace comme to dot.
                     df["Kaive"] = (
                         df["Kaive"]
                         .str.replace(" ", "")
                         .str.replace(",", ".")
                     )
-                    list = file.split("_")
-                    df["aasta"] = list[2]
-                    df["kvartal"] = list[3]
+                    #The file names of the .csv files also have the information about year and period
+                    #so we split the file names and saved the information into a separate column
+                    split_filename = file.split("_")
+                    df["aasta"] = split_filename[2]
+                    df["kvartal"] = split_filename[3]
                     dataframes.append(df)
             except Exception as e:
                 print(f"Error reading file {file}: {e}")
@@ -212,7 +223,8 @@ def tax_and_customs_board_files():
             print("No dataframes to combine.")
             return pd.DataFrame()
 
-    #Third task - creating a new database table
+    #Third task - creating a new database table. Here we are creating a new DB table and inserting
+    #the data from dataframe into the database.
     @task
     def create_duckdb_table(dataframe):
         conn = duckdb.connect("include/turnover_data.db")
@@ -253,13 +265,14 @@ def tax_and_customs_board_files():
 
 tax_and_customs_board_files()
 
-#Third DAG - creating combined fact tabel
+#Third DAG - creating combined fact tabel.
 @dag(start_date=datetime(2023, 6, 1), schedule=None, catchup=False)
 def create_fact_table():
     #First task - joining DB tables and creating combined fact table
     @task
     def join_tables():
         conn = duckdb.connect("include/turnover_data.db")
+        # Faktitabel means fact table
         conn.execute(
             """
             DROP TABLE IF EXISTS faktitabel;
